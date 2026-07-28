02b1beb9efa0690b5c33ebcb988270868265c6c1
Gustavo Santana <gustavo.rsantana@hotmail.com>
2026-07-28 20:14:21 -0300

    Sitema do heroi finalizado

---
+++ Models/Heroi.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SistemaHeroi.Models
+{
+    internal class Heroi
+    {
+        public string nome { get; set; }
+        public string classe { get; set; }
+        public int nivel { get; set; } = 1;
+        public int vida { get; set; } = 100;
+        public int mana { get; set; } = 100;
+        public int ataque { get; set; } = 10;
+        public int defesa { get; set; } = 5;
+
+        public Heroi(string nome, string classe)
+        {
+            this.nome = nome;
+            this.classe = classe;
+        }
+
+
+
+        public void Atacar()
+        {
+            var ataqueTotal = ataque + (nivel * 2);
+            Console.WriteLine($"O herói está atacando com {ataqueTotal} de dano!");
+        }
+
+        public void ReceberDano()
+        {
+            var danoTotal = 10 - defesa;
+            if ( danoTotal > this.vida ) {
+                danoTotal = this.vida - 1;
+                Console.WriteLine("O herói está prestes a morrer!");
+            }
+            Console.WriteLine($"O herói recebeu {danoTotal} de dano!");
+            this.vida -= danoTotal;
+        }
+
+        public void Curar()
+        {
+            Console.WriteLine("O herói está se curando!");
+        }
+        public void SubirNivel()
+        {
+            Console.WriteLine("O herói está subindo de nível!");
+            this.nivel++;
+            this.vida += 20;
+            this.mana += 10;
+            this.ataque++;
+            this.defesa++;
+        }
+
+        public void ExibirStatus()
+        {
+            Console.WriteLine($"Nome: {nome}");
+            Console.WriteLine($"Classe: {classe}");
+            Console.WriteLine($"Nível: {nivel}");
+            Console.WriteLine($"Vida: {vida}");
+            Console.WriteLine($"Mana: {mana}");
+            Console.WriteLine($"Ataque: {ataque}");
+            Console.WriteLine($"Defesa: {defesa}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,50 @@
-﻿Console.WriteLine("Inicial game");
+﻿using SistemaHeroi.Models;
+
+Console.WriteLine("Sistema do Heroi");
+Console.WriteLine("Digite o nome do herói:");
+string nome = Console.ReadLine();
+Console.WriteLine("Qual Classe do herói? (Guerreiro, Mago, Arqueiro)");
+string classe = Console.ReadLine();
+
+Heroi heroi = new(nome, classe);
+
+bool sair = false;
+
+while (!sair)
+{
+    Console.WriteLine("=============");
+    Console.WriteLine("Escolha uma ação:");
+    Console.WriteLine("1 - Atacar");
+    Console.WriteLine("2 - Receber Dano");
+    Console.WriteLine("3 - Curar");
+    Console.WriteLine("4 - Subir de Nível");
+    Console.WriteLine("5 - Exibir Status");
+    Console.WriteLine("6 - Sair");
+    Console.WriteLine("=============");
+
+    string escolha = Console.ReadLine();
+    switch (escolha)
+    {
+        case "1":
+            heroi.Atacar();
+            break;
+        case "2":
+            heroi.ReceberDano();
+            break;
+        case "3":
+            heroi.Curar();
+            break;
+        case "4":
+            heroi.SubirNivel();
+            break;
+        case "5":
+            heroi.ExibirStatus();
+            break;
+        case "6":
+            sair = true;
+            break;
+        default:
+            Console.WriteLine("Opção inválida. Tente novamente.");
+            break;
+    }
+}
